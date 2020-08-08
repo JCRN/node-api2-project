d808428604975f9e0236e6b5d9e83f75f3d679c5
Jason Clemons <clemons.jason@gmail.com>
2020-08-07 21:11:27 -0700

    fixed route in server.js

--- data/db-router.js
+++ data/db-router.js
@@ -26,6 +26,7 @@ router.get('/', (req, res) => {
       res.status(200).json(posts)
     })
     .catch((error) => {
+      console.log(error)
       res.status(500).json({ message: 'Error retrieving the posts' })
     })
 })
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 
 const server = express()
 
-const dbRouter = require('./data/db-router')
+const dbRouter = require('./data/db-router.js')
 
 server.use(express.json())
-server.use('./api/posts', dbRouter)
+server.use('/api/posts', dbRouter)
 
 module.exports = server
