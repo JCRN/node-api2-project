74037db0bf9d1b8eb436f9eeb8350e3e6bff6b40
Jason Clemons <clemons.jason@gmail.com>
2020-08-07 20:58:29 -0700

    added db-router.js for routing

---
+++ data/db-router.js
@@ -0,0 +1,59 @@
+// router object for all /api calls/handlers/middleware/endpoints
+
+const express = require('express')
+
+const db = require('./db.js')
+
+const router = express.Router()
+
+router.post('/', (req, res) => {
+  db.insert(req.body)
+    .then((post) => {
+      res.status(200).json(post)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error adding the post' })
+    })
+})
+
+router.post('/:id/comments', (req, res) => {
+  // something
+})
+
+router.get('/', (req, res) => {
+  db.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error retrieving the posts' })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  // something
+})
+
+router.get('/:id/comments', (req, res) => {
+  db.findPostComments(req.params.id)
+    .then((comments) => {
+      if (comments.length > 0) {
+        res.status(200).json(comments)
+      } else {
+        res.status(404).json({ message: 'No comments found' })
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
+})
+
+router.put('/:id', (req, res) => {
+  // something
+})
+
+router.delete('/:id', (req, res) => {
+  // something
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
-const db = require('./data/db.js')
-
 const server = express()
 
+const dbRouter = require('./data/db-router')
+
 server.use(express.json())
+server.use('./api/posts', dbRouter)
 
 module.exports = server
